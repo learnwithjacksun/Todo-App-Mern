e5fa039214a2269208d12025cdfbc46dccb4f71c
learnwithjacksun <codewithjacksun@gmail.com>
2025-05-30 10:05:15 +0100

    update API base URL and CORS configuration for deployment

--- client/src/Config/axios.ts
+++ client/src/Config/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://todo-backend-bzs4.onrender.com/api",
     withCredentials: true,
 })
 
--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["https://learn-todo-mern.netlify.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
